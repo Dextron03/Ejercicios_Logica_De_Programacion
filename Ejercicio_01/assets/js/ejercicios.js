023ae7152183ccdcba021492c670adf43e65a929
Dextron03 <brailyrs03@gmail.com>
2024-10-21 02:47:43 -1200

    Ejercicio 8 completo.

--- Ejercicio_01/assets/js/ejercicios.js
+++ Ejercicio_01/assets/js/ejercicios.js
@@ -39,6 +39,20 @@ export const esPalindromo = (palabra) => {
 };
 
 
-
 /* 8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. 
 miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5. */
+
+export const eliminarParametro = (cadena) => {
+
+    let arrayDeNumeros = [];
+
+    for (const letra of cadena) {
+        if(isNaN(letra)==false){
+            if(letra != " "){
+                arrayDeNumeros.push(letra);
+            }
+        }
+    }
+
+    return arrayDeNumeros.join(", ");
+};
--- Ejercicio_01/assets/js/main.js
+++ Ejercicio_01/assets/js/main.js
@@ -1,7 +1,10 @@
-import {palabraAlReves, contadorDePalabras,esPalindromo } from "./ejercicios.js"
+import {palabraAlReves, contadorDePalabras,esPalindromo, eliminarParametro} from "./ejercicios.js"
 
 console.log(palabraAlReves("Oso"));
 
 console.log(contadorDePalabras("hola mundo adios mundo", "mundo"));
 
 console.log(esPalindromo("Oso"));
+
+console.log(eliminarParametro("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz"));
+
